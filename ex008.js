06fd567905a5a895b11c8afc341cf91d1ce3be83
ivysonin <ivysonthauan4@gmail.com>
2024-12-06 18:04:12 -0300

    Calculando Àrea

---
+++ ex008.js
@@ -0,0 +1,8 @@
+let base = parseFloat(prompt("Digite a base:"));
+let altura = parseFloat(prompt("Digite a altura:"));
+
+// Calculando a área
+let area = base * altura
+
+// Exibindo bonitinho
+console.log(`\nSabendo que a Base é: ${base}m\nE a altura é: ${altura}m\nTemos a área de ${area}m²`)
