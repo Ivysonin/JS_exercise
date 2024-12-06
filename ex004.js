2f50fff6b1f6c676861f53bb16aa2089ecdd5626
ivysonin <ivysonthauan17@gmail.com>
2024-12-06 18:00:47 -0300

    Antecessor e Sucessor

---
+++ ex004.js
@@ -0,0 +1,5 @@
+let numero = parseInt(prompt("Digite um número:"));
+
+/* Para funcionar, tive que atribuir o sinal de (++) 2 vezes a varival 
+para que ela me fornecesse o resultado esperado. */
+console.log(`O antecessor de ${numero} é ${--numero}\nE o sucessor de ${++numero} é ${++numero}`)
