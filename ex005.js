0f1e6c074f66fade66d7774709b16aee0016b33e
ivysonin <ivysonthauan4@gmail.com>
2024-12-06 18:01:47 -0300

    dobro e triplo

---
+++ ex005.js
@@ -0,0 +1,11 @@
+// Perguntando ao usuario um número.
+let numero = parseInt(prompt("Digite um número;"));
+
+// Fazendo o dobro do número.
+let dobro = numero * 2
+
+// Fazendo o triplo do número.
+let triplo = numero * 3
+
+// Exibindo bonitinho.
+console.log(`O Número que você escolheu: ${numero}\nO Dobro é: ${dobro}\nO Triplo é: ${triplo}`)
