dea931ece2c55dc8d75e68c5cc33bd655ec4060a
ivysonin <ivysonthauan17@gmail.com>
2024-12-10 18:03:48 -0300

    Adivinhando número 'segredo'. (while)

---
+++ ex027.js
@@ -0,0 +1,24 @@
+const numero_segredo = 5;
+let tentativas = 4;
+
+while (tentativas > 0) {
+
+    tentativas--;
+
+    let escolha = parseInt(prompt("Digite um número para adivinhar o número segredo"));
+
+    if (escolha > numero_segredo) {
+        alert(`Número alto, tente um mais baixo\nTentativas: ${tentativas}`);
+    } else if (escolha < numero_segredo) {
+        alert(`Número baixo, tente um mais alto\nTentativas: ${tentativas}`);
+    } else if (escolha === numero_segredo) {
+        console.log(`Você acertou, o número segredo era ${numero_segredo}`);
+        break;
+    } else {
+        alert("Digite números inteiros");
+    }
+}
+
+if (tentativas == 0) {
+    alert("Tentativas acabaram, tente depois !");
+}
